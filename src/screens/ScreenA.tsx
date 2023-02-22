54dfb4f9da103185768ac3cad95170464c9d470e
eduardoaraujomilfont <eduardoaraujomilfont@gmail.com>
2023-02-21 22:54:18 -0300

    Screen A, Screen B e Navegação Simples

---
+++ src/screens/ScreenA.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+
+export function ScreenA({ navigation }) {
+  function openScreen(){ 
+    navigation.navigate('screenB')
+    
+    
+    
+  }
+
+
+  
+  return (
+    <View style={{ flex: 1, backgroundColor: '#eab676', justifyContent: 'center', alignItems: 'center'}}>
+        <Text>Screen A</Text>
+
+        <View style ={{justifyContent: 'center', alignItems: 'center', flexDirection: 'row'}}>
+          <Button
+            title = "Ir para B"
+            onPress={openScreen}
+          />
+
+        </View>     
+    </View>
+  );
+}
